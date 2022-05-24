26b3399943980fd2c6801a7d4496923da42d2197
binggh <binggh@proton.me>
2022-05-24 16:19:15 +0800

    Error handling for tag fetching and moved info messages to more appropriate locations

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::Path;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use clap::Parser;
 use dirs::home_dir;
 use tracing::info;
@@ -19,26 +19,33 @@ pub struct InstallCommand {}
 pub fn install() -> Result<()> {
     info!("Downloading the Forc toolchain");
 
-    let forc_release_latest_tag = get_latest_tag(&format!(
+    let forc_release_latest_tag = match get_latest_tag(&format!(
         "{}{}/{}",
         GITHUB_API_REPOS_BASE_URL, SWAY_REPO, RELEASES_LATEST
-    ))?;
-    let fuel_core_release_latest_tag = get_latest_tag(&format!(
+    )) {
+        Ok(t) => t,
+        Err(_) => bail!("Failed to fetch latest forc release tag from GitHub API"),
+    };
+
+    let fuel_core_release_latest_tag = match get_latest_tag(&format!(
         "{}{}/{}",
         GITHUB_API_REPOS_BASE_URL, FUEL_CORE_REPO, RELEASES_LATEST
-    ))?;
-
-    info!("Fetching forc {}", &forc_release_latest_tag);
-    info!("Fetching fuel-core {}", &fuel_core_release_latest_tag);
+    )) {
+        Ok(t) => t,
+        Err(_) => bail!("Failed to fetch latest fuel-core release tag from GitHub API"),
+    };
 
     let forc_bin_tarball_name = forc_bin_tarball_name()?;
     let fuel_core_bin_tarball_name = fuel_core_bin_tarball_name(&fuel_core_release_latest_tag)?;
 
+    info!("Fetching forc {}", &forc_release_latest_tag);
     download_file_and_unpack(
         SWAY_RELEASE_DOWNLOAD_URL,
         &forc_release_latest_tag,
         &forc_bin_tarball_name,
     )?;
+
+    info!("Fetching fuel-core {}", &fuel_core_release_latest_tag);
     download_file_and_unpack(
         FUEL_CORE_RELEASE_DOWNLOAD_URL,
         &fuel_core_release_latest_tag,

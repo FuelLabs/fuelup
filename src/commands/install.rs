cd210e671cf44800f3cb1c124aae19a7d2e9c258
binggh <binggh@proton.me>
2022-05-27 09:02:18 +0800

    remove newline

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -80,7 +80,7 @@ pub fn install() -> Result<()> {
         "\n\nInstalled: forc {}, fuel-core {}",
         forc_release_latest_tag, fuel_core_release_latest_tag
     );
-    info!("\n\nThe Forc toolchain is installed now. Great!");
+    info!("\nThe Forc toolchain is installed now. Great!");
     info!("\nYou might need to add $HOME/.fuelup/bin to your path.");
 
     Ok(())

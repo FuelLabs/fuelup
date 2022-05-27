95616249214f337b330ed067494c0a652ee6d91e
binggh <binggh@proton.me>
2022-05-27 14:23:45 +0800

    rename FUELUP_PATH -> FUELUP_DIR, remove FUELUP_BIN_PATH

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,4 @@
-pub const FUELUP_PATH: &str = ".fuelup";
-pub const FUELUP_BIN_PATH: &str = ".fuelup/bin";
+pub const FUELUP_DIR: &str = ".fuelup";
 pub const SWAY_REPO: &str = "sway";
 pub const FUEL_CORE_REPO: &str = "fuel-core";
 

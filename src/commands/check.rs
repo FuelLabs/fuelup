65b92762f3f90eeebc140275885d102c92928293
binggh <binggh@proton.me>
2022-06-08 00:02:12 +0800

    clippy

--- src/commands/check.rs
+++ src/commands/check.rs
@@ -14,16 +14,14 @@ pub fn exec() -> Result<()> {
         let output = std::process::Command::new(component)
             .arg("--version")
             .output()
-            .expect(&format!("Could not run {} --version", component))
+            .unwrap_or_else(|_| panic!("Could not run {} --version", component))
             .stdout;
 
         let output = String::from_utf8_lossy(&output);
         let version = "v".to_owned() + output.split_whitespace().collect::<Vec<&str>>()[1];
 
-        let download_cfg: DownloadCfg = DownloadCfg::new(component, None).expect(&format!(
-            "Could not create download config for {}",
-            component
-        ));
+        let download_cfg: DownloadCfg = DownloadCfg::new(component, None)
+            .unwrap_or_else(|_| panic!("Could not create download config for {}", component));
 
         if version == download_cfg.version {
             info!("{} - up to date: {}", component, version);

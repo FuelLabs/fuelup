74dc9e8f93904790c08a9a6bb4a8ff333cdcbcb3
binggh <binggh@proton.me>
2022-06-12 23:50:03 +0800

    proxy mode

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use anyhow::Result;
 use clap::Parser;
-use fuelup::commands::install;
+use dirs::home_dir;
+use fuelup::commands::toolchain;
+use std::ffi::OsString;
+use std::os::unix::prelude::CommandExt;
+use std::path::PathBuf;
+use std::process::{Command, ExitCode, Stdio};
+use std::{env, io, panic};
 
 use fuelup::commands::fuelup::{self_update, FuelupCommand};
-use fuelup::commands::install::InstallCommand;
+use fuelup::commands::toolchain::ToolchainCommand;
+
+mod file;
+mod path;
 
 #[derive(Debug, Parser)]
 #[clap(name = "fuelup", about = "Fuel Toolchain Manager", version)]
@@ -14,14 +23,76 @@ struct Cli {
 
 #[derive(Debug, Parser)]
 enum Commands {
-    /// Installs the latest Fuel toolchain.
-    Install(InstallCommand),
     /// Manage your fuelup installation.
     #[clap(name = "self", subcommand)]
     Fuelup(FuelupCommand),
+    /// Install new toolchains or modify/query installed toolchains
+    #[clap(subcommand, alias = "install")]
+    Toolchain(ToolchainCommand),
 }
 
-fn main() -> Result<()> {
+fn is_supported_component(component: &str) -> bool {
+    ["forc", "fuel-core", "forc-fmt", "forc-lsp", "forc-explore"].contains(&component)
+}
+
+/// Runs forc or fuel-core in proxy mode
+fn proxy_run(arg0: &str) {
+    let cmd_args: Vec<_> = env::args_os().skip(1).collect();
+
+    direct_proxy(arg0, &cmd_args);
+}
+
+fn direct_proxy(arg0: &str, args: &[OsString]) -> io::Result<ExitCode> {
+    let bin_path = home_dir()
+        .unwrap()
+        .join(".fuelup/toolchains/latest-x86_64-apple-darwin/bin")
+        .join(arg0);
+    let mut cmd = Command::new(bin_path);
+
+    cmd.args(args);
+    cmd.stdin(Stdio::inherit());
+
+    return exec(&mut cmd);
+
+    fn exec(cmd: &mut Command) -> io::Result<ExitCode> {
+        Err(cmd.exec())
+    }
+}
+
+fn run() -> Result<()> {
+    let arg0 = std::env::args().next().map(PathBuf::from);
+
+    let process_name = arg0
+        .as_ref()
+        .and_then(|a| a.file_stem())
+        .and_then(std::ffi::OsStr::to_str)
+        .map(String::from);
+
+    match process_name.as_deref() {
+        Some("fuelup") => fuelup_cli()?,
+        Some(n) => {
+            println!("{:?}", n);
+            if is_supported_component(n) {
+                proxy_run(n);
+            }
+        }
+        None => panic!("Unknown exe"),
+    }
+    Ok(())
+}
+
+fn fuelup_cli() -> Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Fuelup(command) => match command {
+            FuelupCommand::Update => self_update(),
+        },
+        Commands::Toolchain(command) => toolchain::exec(command),
+    }
+}
+
+fn main() {
     let format = tracing_subscriber::fmt::format()
         .without_time()
         .with_level(false)
@@ -29,12 +100,7 @@ fn main() -> Result<()> {
 
     tracing_subscriber::fmt().event_format(format).init();
 
-    let cli = Cli::parse();
-
-    match cli.command {
-        Commands::Install(command) => install::exec(command),
-        Commands::Fuelup(command) => match command {
-            FuelupCommand::Update => self_update(),
-        },
+    if let Err(_) = run() {
+        std::process::exit(1);
     }
 }

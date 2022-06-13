1f04587547b629de152a08ea7755770bd827353e
binggh <binggh@proton.me>
2022-06-13 19:33:47 +0800

    Add handlers for file write/read

--- src/file.rs
+++ src/file.rs
@@ -23,3 +23,25 @@ pub fn hardlink_file(src: &Path, dest: &Path) -> Result<()> {
 fn symlink_file(src: &Path, dest: &Path) -> Result<()> {
     std::os::unix::fs::symlink(src, dest).with_context(|| "Could not create link".to_string())
 }
+
+pub fn is_file<P: AsRef<Path>>(path: P) -> bool {
+    fs::metadata(path).ok().as_ref().map(fs::Metadata::is_file) == Some(true)
+}
+
+pub fn read_file(name: &'static str, path: &Path) -> Result<String> {
+    fs::read_to_string(path).with_context(|| format!("Failed to read {}", name))
+}
+
+pub fn write_file(path: &Path, contents: &str) -> io::Result<()> {
+    let mut file = fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(path)?;
+
+    io::Write::write_all(&mut file, contents.as_bytes())?;
+
+    file.sync_data()?;
+
+    Ok(())
+}

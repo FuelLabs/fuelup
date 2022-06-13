05f1ecdc2be1cf298f2f59b84d95a98486e0fb01
binggh <binggh@proton.me>
2022-06-13 19:34:43 +0800

    Update toolchain path and add settings path

--- src/path.rs
+++ src/path.rs
@@ -4,14 +4,26 @@ use dirs;
 
 pub const FUELUP_DIR: &str = ".fuelup";
 
+pub fn fuelup_dir() -> PathBuf {
+    dirs::home_dir().unwrap().join(FUELUP_DIR)
+}
+
 pub fn fuelup_bin_dir() -> PathBuf {
     dirs::home_dir().unwrap().join(FUELUP_DIR).join("bin")
+}
+
+pub fn settings_file() -> PathBuf {
+    dirs::home_dir()
+        .unwrap()
+        .join(FUELUP_DIR)
+        .join("settings.toml")
 }
 
 pub fn toolchain_bin_dir(toolchain: &str) -> PathBuf {
     dirs::home_dir()
         .unwrap()
         .join(FUELUP_DIR)
+        .join("toolchains")
         .join(toolchain)
         .join("bin")
 }

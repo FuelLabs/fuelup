4abe8e494591c225d8cba10cfd228a1958abd670
binggh <binggh@proton.me>
2022-06-19 16:07:10 +0800

    add bold

--- src/colors.rs
+++ src/colors.rs
@@ -1,12 +1,30 @@
+use anyhow::Result;
 use std::io::Write;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
-pub fn print_with_color(txt: &str, color: Color) {
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    stdout
-        .set_color(ColorSpec::new().set_fg(Some(color)).set_bold(true))
-        .expect("internal printing error");
+pub fn print_boldln(txt: &str) -> Result<()> {
+    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+    stdout.set_color(ColorSpec::new().set_bold(true))?;
 
-    write!(&mut stdout, "{}", txt).expect("internal printing error");
-    stdout.reset().expect("internal printing error");
+    writeln!(&mut stdout, "{}", txt)?;
+    stdout.reset()?;
+    Ok(())
+}
+
+pub fn print_bold(txt: &str) -> Result<()> {
+    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+    stdout.set_color(ColorSpec::new().set_bold(true))?;
+
+    write!(&mut stdout, "{}", txt)?;
+    stdout.reset()?;
+    Ok(())
+}
+
+pub fn print_with_color(txt: &str, color: Color) -> Result<()> {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    stdout.set_color(ColorSpec::new().set_fg(Some(color)).set_bold(true))?;
+
+    write!(&mut stdout, "{}", txt)?;
+    stdout.reset()?;
+    Ok(())
 }

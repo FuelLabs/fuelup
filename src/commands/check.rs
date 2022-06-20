e96a2bf6b47781f4b43a0242d6eb1a0bb1720fe0
binggh <binggh@proton.me>
2022-06-20 09:32:03 +0800

    Remove unused code

--- src/commands/check.rs
+++ src/commands/check.rs
@@ -6,12 +6,6 @@ use crate::ops::fuelup_check;
 #[derive(Debug, Parser)]
 pub struct CheckCommand {}
 
-pub mod plugin {
-    pub const FMT: &str = "fmt";
-    pub const LSP: &str = "lsp";
-    pub const EXPLORE: &str = "explore";
-}
-
 pub fn exec() -> Result<()> {
     fuelup_check::check()?;
 
--- src/ops/fuelup_check.rs
+++ src/ops/fuelup_check.rs
@@ -14,12 +14,6 @@ use crate::{component, download::DownloadCfg};
 #[derive(Debug, Parser)]
 pub struct CheckCommand {}
 
-pub mod plugin {
-    pub const FMT: &str = "fmt";
-    pub const LSP: &str = "lsp";
-    pub const EXPLORE: &str = "explore";
-}
-
 pub fn check() -> Result<()> {
     let cfg = Config::from_env()?;
     let toolchains = cfg.list_toolchains()?;
@@ -28,7 +22,6 @@ pub fn check() -> Result<()> {
         if let Ok(toolchain) = Toolchain::from(&toolchain) {
             print_boldln(&toolchain.name)?;
             for component in [component::FORC, component::FUEL_CORE] {
-                let mut latest_version: String = String::new();
                 let component_executable = toolchain.path.join(component);
 
                 match std::process::Command::new(component_executable)
@@ -44,11 +37,14 @@ pub fn check() -> Result<()> {
 
                         let download_cfg: DownloadCfg = DownloadCfg::new(component, None)?;
                         if component == component::FORC {
-                            latest_version = download_cfg.version.to_string();
+                            print_bold(&format!(
+                                "  {} (plugins: forc-explore, forc-fmt, forc-lsp) - ",
+                                component
+                            ))?;
+                        } else {
+                            print_bold(&format!("  {} - ", component))?;
                         }
 
-                        print_bold(&format!("  {} - ", component))?;
-                        println!("{}", version);
                         if version == download_cfg.version {
                             print_with_color("Up to date ", Color::Green)?;
                             println!(": {}", version);
@@ -62,40 +58,6 @@ pub fn check() -> Result<()> {
                         info!(" not found");
                     }
                 };
-
-                if component == component::FORC {
-                    for plugin in [plugin::FMT, plugin::LSP, plugin::EXPLORE] {
-                        let plugin_component = component.to_owned() + "-" + plugin;
-                        match std::process::Command::new(&plugin_component)
-                            .arg("--version")
-                            .output()
-                        {
-                            Ok(o) => {
-                                let version = Version::parse(
-                                    String::from_utf8_lossy(&o.stdout)
-                                        .split_whitespace()
-                                        .collect::<Vec<&str>>()[1],
-                                )?;
-
-                                if version == Version::parse(&latest_version)? {
-                                    print!("    - {} - ", plugin_component);
-                                    print_with_color("Up to date ", Color::Green)
-                                        .expect("Internal printing error");
-                                    println!(": {}", version);
-                                } else {
-                                    print!("    - {} - ", plugin_component);
-                                    print_with_color("Update available ", Color::Yellow)
-                                        .expect("Internal printing error");
-                                    println!("{} -> {}", version, latest_version);
-                                }
-                            }
-                            Err(_) => {
-                                print_bold(&format!("  {}", &plugin_component))?;
-                                info!(" not found");
-                            }
-                        }
-                    }
-                }
             }
         }
     }
@@ -112,7 +74,7 @@ pub fn check() -> Result<()> {
                     .collect::<Vec<&str>>()[1],
             )?;
 
-            print_bold(&format!("  {} - ", component::FUELUP))?;
+            print_bold(&format!("{} - ", component::FUELUP))?;
             if version == download_cfg.version {
                 print_with_color("Up to date ", Color::Green)?;
                 println!(": {}", version);

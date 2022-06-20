7bc8d7c2defc9c5933343c98aa120f9c0550e6d8
binggh <binggh@proton.me>
2022-06-20 16:45:15 +0800

    use ColorChoice::Auto instead of ColorChoice::Always

--- src/fmt.rs
+++ src/fmt.rs
@@ -16,7 +16,7 @@ pub fn with_color_maybe_bold<F>(write: F, color: Color, bold: bool) -> Result<()
 where
     F: FnOnce(&mut StandardStream) -> std::io::Result<()>,
 {
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
     stdout.set_color(ColorSpec::new().set_fg(Some(color)).set_bold(bold))?;
     write(&mut stdout)?;
     stdout.reset()?;

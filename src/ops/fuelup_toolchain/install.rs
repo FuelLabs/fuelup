3624502837ec232065a2ccda9348b593e7a08a79
bingcicle <binggh@proton.me>
2022-07-27 20:11:29 +0800

    Split fuelup_toolchain into install and new

---
+++ src/ops/fuelup_toolchain/install.rs
@@ -0,0 +1,72 @@
+use crate::component;
+use crate::download::{target_from_name, DownloadCfg};
+use crate::path::settings_file;
+use crate::settings::SettingsFile;
+use crate::toolchain::{DistToolchainName, Toolchain};
+use crate::{channel::Channel, commands::toolchain::InstallCommand};
+use anyhow::Result;
+use std::fmt::Write;
+use tracing::{error, info};
+
+pub mod toolchain {
+    pub const LATEST: &str = "latest";
+}
+
+pub fn install(command: InstallCommand) -> Result<()> {
+    let InstallCommand { name } = command;
+
+    let toolchain = Toolchain::new(&name, None)?;
+
+    let settings = SettingsFile::new(settings_file());
+    settings.with_mut(|s| {
+        s.default_toolchain = Some(toolchain.name.clone());
+        Ok(())
+    })?;
+
+    let mut errored_bins = String::new();
+    let mut installed_bins = String::new();
+
+    let cfgs: Vec<DownloadCfg> = match Channel::from_dist_channel(&DistToolchainName::Latest) {
+        Ok(c) => c.build_download_configs(),
+        Err(e) => {
+            error!(
+                "Failed to get latest channel {} - fetching versions using GitHub API",
+                e
+            );
+            [component::FORC, component::FUEL_CORE, component::FORC_LSP]
+                .iter()
+                .map(|c| {
+                    DownloadCfg::new(c, target_from_name(c).ok(), None)
+                        .expect("Failed to create DownloadCfg from component")
+                })
+                .collect()
+        }
+    };
+
+    info!(
+        "Downloading: {}",
+        cfgs.iter()
+            .map(|c| c.name.clone() + " ")
+            .collect::<String>()
+    );
+    for cfg in cfgs {
+        match toolchain.add_component(cfg) {
+            Ok(cfg) => writeln!(installed_bins, "- {} {}", cfg.name, cfg.version)?,
+            Err(e) => writeln!(errored_bins, "- {}", e)?,
+        };
+    }
+
+    if errored_bins.is_empty() {
+        info!("\nInstalled:\n{}", installed_bins);
+        info!("\nThe Fuel toolchain is installed and up to date");
+    } else if installed_bins.is_empty() {
+        error!("\nfuelup failed to install:\n{}", errored_bins)
+    } else {
+        info!(
+            "\nThe Fuel toolchain is partially installed.\nfuelup failed to install: {}",
+            errored_bins
+        );
+    };
+
+    Ok(())
+}
---
+++ src/ops/fuelup_toolchain/new.rs
@@ -0,0 +1,28 @@
+use crate::path::ensure_dir_exists;
+use crate::{commands::toolchain::NewCommand, path::toolchain_dir};
+use anyhow::bail;
+use anyhow::Result;
+use std::fs;
+use std::io;
+
+pub fn new(command: NewCommand) -> Result<()> {
+    let NewCommand { name } = command;
+
+    let toolchain_dir = toolchain_dir();
+
+    let toolchains: Vec<String> = fs::read_dir(&toolchain_dir)?
+        .filter_map(io::Result::ok)
+        .filter(|e| e.file_type().map(|f| f.is_dir()).unwrap_or(false))
+        .map(|e| e.file_name().into_string().ok().unwrap_or_default())
+        .collect();
+
+    if toolchains.contains(&name) {
+        bail!("Toolchain with name '{}' already exists", &name)
+    }
+
+    if ensure_dir_exists(&toolchain_dir.join(&name)).is_ok() {
+        println!("New toolchain initialized: {}", &name);
+    };
+
+    Ok(())
+}

f1f6e97b46c6022bc44bf00eac6b075f0158ba44
bingcicle <binggh@proton.me>
2022-08-01 11:58:23 +0800

    Implement add

--- src/ops/fuelup_component/add.rs
+++ src/ops/fuelup_component/add.rs
@@ -1,9 +1,57 @@
-use anyhow::Result;
+use std::str::FromStr;
 
-use crate::commands::component::AddCommand;
+use anyhow::{bail, Result};
+use semver::Version;
+
+use crate::{
+    commands::component::AddCommand,
+    download::{target_from_name, DownloadCfg},
+    path::settings_file,
+    settings::SettingsFile,
+    toolchain::{DistToolchainName, Toolchain},
+};
 
 pub fn add(command: AddCommand) -> Result<()> {
-    let AddCommand { component } = command;
-    println!("Add component {}", component);
+    let AddCommand {
+        maybe_versioned_component,
+    } = command;
+
+    let settings = SettingsFile::new(settings_file());
+    let current_default = match settings.with(|s| Ok(s.default_toolchain.clone()))? {
+        Some(t) => t,
+        None => {
+            bail!("No default toolchain detected. Please install or create a toolchain first.")
+        }
+    };
+
+    let toolchain = Toolchain::from(&current_default)?;
+    if let Some((component, version)) = maybe_versioned_component.split_once('@') {
+        if let Ok(toolchain) = DistToolchainName::from_str(&current_default) {
+            bail!(
+                "You cannot specify versions of components to add to official toolchain '{}'",
+                toolchain
+            );
+        }
+
+        println!("installing component {} to {}", component, &current_default);
+        let download_cfg = DownloadCfg::new(
+            component,
+            Some(target_from_name(component)?),
+            Some(Version::from_str(version)?),
+        )?;
+        toolchain.add_component(download_cfg)?;
+    } else {
+        println!(
+            "installing component {} to {}",
+            maybe_versioned_component, &current_default
+        );
+        let download_cfg = DownloadCfg::new(
+            &maybe_versioned_component,
+            Some(target_from_name(&maybe_versioned_component)?),
+            None,
+        )?;
+        toolchain.add_component(download_cfg)?;
+    }
+
     Ok(())
 }

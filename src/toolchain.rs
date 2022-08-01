42ce69b540ce5ac5d251f127b2309af08de36541
bingcicle <binggh@proton.me>
2022-08-01 11:59:37 +0800

    Some refactoring to src/toolchain.rs

--- src/toolchain.rs
+++ src/toolchain.rs
@@ -8,24 +8,45 @@ use crate::download::{download_file_and_unpack, link_to_fuelup, unpack_bins, Dow
 use crate::ops::fuelup_self::self_update;
 use crate::path::{ensure_dir_exists, fuelup_bin, fuelup_bin_dir, toolchain_bin_dir};
 
-pub mod toolchain {
-    pub const LATEST: &str = "latest";
-}
+pub const RESERVED_TOOLCHAIN_NAMES: &[&str] = &["latest", "nightly"];
+pub const LATEST: &str = "latest";
 
 pub enum DistToolchainName {
     Latest,
 }
 
+impl fmt::Display for DistToolchainName {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            DistToolchainName::Latest => write!(f, "latest"),
+        }
+    }
+}
+
 impl FromStr for DistToolchainName {
     type Err = anyhow::Error;
     fn from_str(s: &str) -> Result<Self> {
-        let split = s.split_once('-').unwrap();
-        let name = split.0;
+        let name = s.split_once('-').and_then(|n| Some(n.0));
         match name {
-            "latest" => Ok(Self::Latest),
+            Some("latest") => Ok(Self::Latest),
             _ => bail!("Unknown name for toolchain: {}", s),
         }
     }
+}
+
+pub fn is_official_toolchain(toolchain: &str) -> bool {
+    let mut reserved: Vec<String> = Vec::new();
+    let triple = TargetTriple::from_host().ok();
+    for reserved_toolchain_name in RESERVED_TOOLCHAIN_NAMES {
+        reserved.push(reserved_toolchain_name.to_string());
+
+        if triple.is_some() {
+            reserved
+                .push(reserved_toolchain_name.to_string() + &triple.as_ref().unwrap().to_string());
+        }
+    }
+
+    reserved.contains(&toolchain.to_string())
 }
 
 #[derive(Debug)]
@@ -91,6 +112,10 @@ impl Toolchain {
         })
     }
 
+    pub fn exists(&self) -> bool {
+        self.path.exists() && self.path.is_dir()
+    }
+
     pub fn add_component(&self, download_cfg: DownloadCfg) -> Result<DownloadCfg> {
         // Pre-install checks: ensuring toolchain dir, fuelup bin dir, and fuelup exist
         ensure_dir_exists(&self.path)?;
@@ -106,7 +131,10 @@ impl Toolchain {
             };
         }
 
-        info!("Fetching {} {}", &download_cfg.name, &download_cfg.version);
+        info!(
+            "Installing component {} v{}",
+            &download_cfg.name, &download_cfg.version
+        );
 
         if let Err(e) = download_file_and_unpack(&download_cfg, &self.path) {
             bail!(

b91e8b7f32cb311afd4b6accfd7a0e0dd002108a
bingcicle <binggh@proton.me>
2022-08-04 15:46:51 +0800

    First integration tests

---
+++ tests/install.rs
@@ -0,0 +1,49 @@
+use anyhow::Result;
+use std::{env, os::unix::prelude::CommandExt};
+
+mod workdir;
+
+#[test]
+fn smoke_test() -> Result<()> {
+    workdir::setup(&|cfg| {
+        let expected_version = format!("fuelup {}\n", clap::crate_version!());
+
+        let output = cfg.cmd.arg("--version").output().unwrap();
+        let stdout = String::from_utf8_lossy(&output.stdout);
+
+        assert_eq!(stdout, expected_version);
+    })?;
+
+    Ok(())
+}
+
+#[test]
+fn fuelup_toolchain_install_latest() -> Result<()> {
+    workdir::setup(&|cfg| {
+        cfg.cmd
+            .args(["toolchain", "install", "latest"])
+            .output()
+            .expect("Failed to run command");
+
+        let expected_bins = ["forc", "forc-explore", "fuel-core", "forc-lsp", "forc-fmt"];
+
+        for entry in cfg.toolchains_dir().read_dir().expect("Could not read dir") {
+            let toolchain_dir = entry.unwrap();
+            assert_eq!("latest-x86_64-apple-darwin", toolchain_dir.file_name());
+            assert!(toolchain_dir.file_type().unwrap().is_dir());
+
+            let downloaded_bins: Vec<String> = toolchain_dir
+                .path()
+                .join("bin")
+                .read_dir()
+                .expect("Could not read toolchain bin dir")
+                .into_iter()
+                .map(|b| b.unwrap().file_name().to_string_lossy().to_string())
+                .collect();
+
+            assert_eq!(downloaded_bins, expected_bins);
+        }
+    })?;
+
+    Ok(())
+}
---
+++ tests/workdir.rs
@@ -0,0 +1,62 @@
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+    process::Command,
+};
+
+use anyhow::Result;
+use tempfile::tempdir_in;
+
+pub struct TestCfg {
+    pub cmd: Command,
+    pub root: PathBuf,
+    pub home: PathBuf,
+}
+
+impl TestCfg {
+    pub fn new(cmd: Command, root: PathBuf, home: PathBuf) -> Self {
+        Self { cmd, root, home }
+    }
+
+    pub fn fuelup_dir(&self) -> PathBuf {
+        return self.home.join(".fuelup");
+    }
+
+    pub fn toolchains_dir(&self) -> PathBuf {
+        return self.home.join(".fuelup").join("toolchains");
+    }
+}
+
+/// Setup an empty work directory and return a command pointing to the ripgrep
+/// executable whose CWD is set to the work directory.
+///
+/// The name given will be used to create the directory. Generally, it should
+/// correspond to the test name.
+pub fn setup(f: &dyn Fn(&mut TestCfg)) -> Result<()> {
+    let root = env::current_exe()
+        .unwrap()
+        .parent()
+        .expect("fuelup's directory")
+        .to_path_buf();
+    let testdir = tempdir_in(&root).unwrap();
+    let tmp_home = testdir.path();
+
+    let tmp_fuelup_root_path = tmp_home.join(".fuelup");
+    let tmp_fuelup_bin_dir_path = tmp_home.join(".fuelup").join("bin");
+    fs::create_dir(&tmp_fuelup_root_path).unwrap();
+    fs::create_dir(&tmp_fuelup_bin_dir_path).unwrap();
+    fs::create_dir(&tmp_fuelup_root_path.join("toolchains")).unwrap();
+
+    let bin = root.parent().unwrap().join("fuelup");
+    fs::copy(bin, &tmp_fuelup_bin_dir_path.join("fuelup"))?;
+
+    println!("fuelup root: {:?}", tmp_fuelup_root_path);
+
+    for entry in tmp_fuelup_root_path.read_dir()? {
+        println!("fuelup entry: {:?}", entry);
+    }
+    env::set_var("HOME", tmp_home);
+    let cmd = Command::new(tmp_fuelup_bin_dir_path.join("fuelup"));
+    f(&mut TestCfg::new(cmd, root, tmp_home.to_path_buf()));
+    Ok(())
+}

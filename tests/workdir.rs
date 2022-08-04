0aefc53c8edad1c4aad58e64a5687cac62ba067a
bingcicle <binggh@proton.me>
2022-08-04 16:08:04 +0800

    Remove printlns

--- tests/workdir.rs
+++ tests/workdir.rs
@@ -54,11 +54,6 @@ pub fn setup(f: &dyn Fn(&mut TestCfg)) -> Result<()> {
     let bin = root.parent().unwrap().join("fuelup");
     fs::copy(bin, &tmp_fuelup_bin_dir_path.join("fuelup"))?;
 
-    println!("fuelup root: {:?}", tmp_fuelup_root_path);
-
-    for entry in tmp_fuelup_root_path.read_dir()? {
-        println!("fuelup entry: {:?}", entry);
-    }
     env::set_var("HOME", tmp_home);
     let cmd = Command::new(tmp_fuelup_bin_dir_path.join("fuelup"));
     f(&mut TestCfg::new(cmd, root, tmp_home.to_path_buf()));

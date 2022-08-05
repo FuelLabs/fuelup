349c05b57d30b261f16ef8e921b9da0a4a4489c5
bingcicle <binggh@proton.me>
2022-08-05 10:12:07 +0800

    Fix self update test

--- tests/commands.rs
+++ tests/commands.rs
@@ -61,6 +61,19 @@ fn fuelup_check() -> Result<()> {
 }
 
 #[test]
+fn fuelup_self_update() -> Result<()> {
+    testcfg::setup(FuelupState::LatestToolchainInstalled, &|cfg| {
+        let output = cfg.exec_cmd(&["self", "update"]);
+        let stdout = String::from_utf8_lossy(&output.stdout);
+
+        let expected_stdout_starts_with = "Fetching binary from";
+        assert!(stdout.starts_with(expected_stdout_starts_with));
+    })?;
+
+    Ok(())
+}
+
+#[test]
 fn fuelup_default() -> Result<()> {
     testcfg::setup(FuelupState::LatestToolchainInstalled, &|cfg| {
         let output = cfg.exec_cmd(&["default"]);

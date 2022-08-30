df42001f04ffa61467a278056a98cd9d87791756
bingcicle <binggh@proton.me>
2022-08-30 15:18:26 +0800

    Fix behaviour with default

--- src/commands/default.rs
+++ src/commands/default.rs
@@ -1,4 +1,6 @@
-use crate::toolchain::RESERVED_TOOLCHAIN_NAMES;
+use std::str::FromStr;
+
+use crate::toolchain::OfficialToolchainDescription;
 use anyhow::{bail, Result};
 use clap::Parser;
 use tracing::info;
@@ -26,10 +28,10 @@ pub fn exec(command: DefaultCommand) -> Result<()> {
 
     let mut new_default = Toolchain::from(&toolchain)?;
 
-    if RESERVED_TOOLCHAIN_NAMES.contains(&toolchain.as_str()) {
+    if let Ok(_) = OfficialToolchainDescription::from_str(&toolchain) {
         new_default = Toolchain::new(&toolchain)?;
-    } else if !new_default.path.exists() {
-        bail!("Toolchain with name '{}' does not exist", &new_default.name)
+    } else if !new_default.exists() {
+        bail!("Toolchain with name '{}' does not exist", &new_default.name);
     };
 
     let settings = SettingsFile::new(settings_file());

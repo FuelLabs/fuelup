c9b62ef65ea3ffe12653e9a60d85dd294e1c6dda
bingcicle <binggh@proton.me>
2022-09-01 09:37:44 +0800

    Disallow adding/removing specific components from official toolchains

--- src/ops/fuelup_component/remove.rs
+++ src/ops/fuelup_component/remove.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 
 use crate::{commands::component::RemoveCommand, toolchain::Toolchain};
 
@@ -6,6 +6,17 @@ pub fn remove(command: RemoveCommand) -> Result<()> {
     let RemoveCommand { component } = command;
 
     let toolchain = Toolchain::from_settings()?;
+
+    if toolchain.is_official() {
+        bail!(
+            "Installing specific components is reserved for custom toolchains.
+You are currently using '{}'.
+
+You may create a custom toolchain using 'fuelup toolchain new <toolchain>'.",
+            toolchain.name
+        )
+    };
+
     toolchain.remove_component(&component)?;
     Ok(())
 }

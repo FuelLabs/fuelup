a0f156f4209fe0094b90e947d09f93aa3592799c
bingcicle <binggh@proton.me>
2022-09-06 13:29:49 +0800

    correct plugin direct_proxy behaviour

--- src/proxy_cli.rs
+++ src/proxy_cli.rs
@@ -4,6 +4,7 @@ use std::os::unix::prelude::CommandExt;
 use std::process::{Command, ExitCode, Stdio};
 use std::{env, io};
 
+use crate::component::SUPPORTED_PLUGINS;
 use crate::toolchain::Toolchain;
 
 /// Runs forc or fuel-core in proxy mode
@@ -11,11 +12,18 @@ pub fn proxy_run(arg0: &str) -> Result<ExitCode> {
     let cmd_args: Vec<_> = env::args_os().skip(1).collect();
     let toolchain = Toolchain::from_settings()?;
 
-    direct_proxy(arg0, &cmd_args, toolchain)?;
+    if !cmd_args.is_empty() {
+        let plugin = format!("{}-{}", arg0, &cmd_args[0].to_string_lossy());
+        if SUPPORTED_PLUGINS.contains(&plugin.as_str()) {
+            direct_proxy(&plugin, &cmd_args[1..], &toolchain)?;
+        }
+    }
+
+    direct_proxy(arg0, &cmd_args, &toolchain)?;
     Ok(ExitCode::SUCCESS)
 }
 
-fn direct_proxy(proc_name: &str, args: &[OsString], toolchain: Toolchain) -> io::Result<ExitCode> {
+fn direct_proxy(proc_name: &str, args: &[OsString], toolchain: &Toolchain) -> io::Result<ExitCode> {
     let bin_path = toolchain.bin_path.join(proc_name);
     let mut cmd = Command::new(bin_path);
 

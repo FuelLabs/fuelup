9254559cce62dc2e9920dd24b30a1934844cd569
bingcicle <bingcicle@proton.me>
2022-09-21 10:26:11 +0800

    remove API related constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,13 +1,5 @@
 use time::{format_description::FormatItem, macros::format_description};
 
-pub const SWAY_REPO: &str = "sway";
-pub const FUEL_CORE_REPO: &str = "fuel-core";
-pub const FUELUP_REPO: &str = "fuelup";
-
-pub const GITHUB_API_REPOS_BASE_URL: &str = "https://api.github.com/repos/FuelLabs/";
-pub const RELEASES_LATEST: &str = "releases/latest";
-pub const RELEASES_TAGS: &str = "releases/tags";
-
 pub const SWAY_RELEASE_DOWNLOAD_URL: &str = "https://github.com/FuelLabs/sway/releases/download";
 pub const FUELUP_RELEASE_DOWNLOAD_URL: &str =
     "https://github.com/FuelLabs/fuelup/releases/download";

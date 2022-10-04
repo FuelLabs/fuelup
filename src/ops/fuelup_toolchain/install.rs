a35e3c025c4afc3551c76a4e9ad706cc86031dcf
bingcicle <bingcicle@proton.me>
2022-10-04 17:50:41 +0800

    remove usage of tempdir in toolchain installation

--- src/ops/fuelup_toolchain/install.rs
+++ src/ops/fuelup_toolchain/install.rs
@@ -1,14 +1,12 @@
 use crate::constants::{CHANNEL_LATEST_FILE_NAME, CHANNEL_NIGHTLY_FILE_NAME};
 use crate::download::DownloadCfg;
-use crate::path::{fuelup_dir, settings_file};
+use crate::path::settings_file;
 use crate::settings::SettingsFile;
 use crate::toolchain::{DistToolchainName, OfficialToolchainDescription, Toolchain};
 use crate::{channel::Channel, commands::toolchain::InstallCommand};
 use anyhow::{bail, Result};
 use std::fmt::Write;
-use std::fs;
 use std::str::FromStr;
-use tempfile::tempdir_in;
 use tracing::{error, info};
 
 pub fn install(command: InstallCommand) -> Result<()> {
@@ -26,49 +24,38 @@ pub fn install(command: InstallCommand) -> Result<()> {
     let mut errored_bins = String::new();
     let mut installed_bins = String::new();
 
-    let fuelup_dir = fuelup_dir();
-    let tmp_dir = tempdir_in(&fuelup_dir)?;
-    let tmp_dir_path = tmp_dir.into_path();
-
-    let cfgs: Vec<DownloadCfg> =
-        if let Ok(channel) = Channel::from_dist_channel(&description, tmp_dir_path.clone()) {
+    {
+        let cfgs: Vec<DownloadCfg> = if let Ok(channel) = Channel::from_dist_channel(&description) {
             channel.build_download_configs()
         } else {
             bail!("Could not build download configs from channel")
         };
 
-    info!(
-        "Downloading: {}",
-        cfgs.iter()
-            .map(|c| c.name.clone() + " ")
-            .collect::<String>()
-    );
-    for cfg in cfgs {
-        match toolchain.add_component(cfg) {
-            Ok(cfg) => writeln!(installed_bins, "- {} {}", cfg.name, cfg.version)?,
-            Err(e) => writeln!(errored_bins, "- {}", e)?,
+        info!(
+            "Downloading: {}",
+            cfgs.iter()
+                .map(|c| c.name.clone() + " ")
+                .collect::<String>()
+        );
+        for cfg in cfgs {
+            match toolchain.add_component(cfg) {
+                Ok(cfg) => writeln!(installed_bins, "- {} {}", cfg.name, cfg.version)?,
+                Err(e) => writeln!(errored_bins, "- {}", e)?,
+            };
+        }
+
+        if errored_bins.is_empty() {
+            info!("\nInstalled:\n{}", installed_bins);
+            info!("\nThe Fuel toolchain is installed and up to date");
+        } else if installed_bins.is_empty() {
+            error!("\nfuelup failed to install:\n{}", errored_bins)
+        } else {
+            info!(
+                "\nThe Fuel toolchain is partially installed.\nfuelup failed to install: {}",
+                errored_bins
+            );
         };
     }
-
-    let channel_file_name = match description.name {
-        DistToolchainName::Latest => CHANNEL_LATEST_FILE_NAME,
-        DistToolchainName::Nightly => CHANNEL_NIGHTLY_FILE_NAME,
-    };
-    if errored_bins.is_empty() {
-        fs::copy(
-            tmp_dir_path.join(channel_file_name),
-            toolchain.path.join(channel_file_name),
-        )?;
-        info!("\nInstalled:\n{}", installed_bins);
-        info!("\nThe Fuel toolchain is installed and up to date");
-    } else if installed_bins.is_empty() {
-        error!("\nfuelup failed to install:\n{}", errored_bins)
-    } else {
-        info!(
-            "\nThe Fuel toolchain is partially installed.\nfuelup failed to install: {}",
-            errored_bins
-        );
-    };
 
     Ok(())
 }

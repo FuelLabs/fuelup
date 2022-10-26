55a9677047012c3a3e3c919b2208cc828d16f74d
bing <bingcicle@proton.me>
2022-10-26 02:38:31 -0400

    better messages for fuelup update (#246)
    * better messages for fuelup update
    
    * rename variables for clarity

--- src/ops/fuelup_update.rs
+++ src/ops/fuelup_update.rs
@@ -1,25 +1,35 @@
 use crate::{
     channel::Channel,
     config::Config,
+    fmt::{bold, colored_bold},
     toolchain::{OfficialToolchainDescription, Toolchain},
 };
 use anyhow::{bail, Result};
-use std::fmt::Write;
+use std::io::Write;
 use std::str::FromStr;
-use tracing::{error, info};
+use termcolor::Color;
+use tracing::info;
+
+const UPDATED: &str = "updated";
+const PARTIALLY_UPDATED: &str = "partially updated";
+const UNCHANGED: &str = "unchanged";
 
 pub fn update() -> Result<()> {
     let config = Config::from_env()?;
+    let toolchains = config.list_official_toolchains()?;
+    let mut summary: Vec<(String, String)> = Vec::with_capacity(toolchains.len());
 
-    for toolchain in config.list_official_toolchains()? {
-        let mut errored_bins = String::new();
+    for toolchain in toolchains {
         let mut installed_bins = String::new();
+        let mut errored_bins = String::new();
 
         let description = OfficialToolchainDescription::from_str(&toolchain)?;
+        info!("updating the '{}' toolchain", description);
 
         let (cfgs, hash) = if let Ok((channel, hash)) = Channel::from_dist_channel(&description) {
             if let Ok(true) = config.hash_matches(&description, &hash) {
-                info!("'{}' is already installed and up to date", toolchain);
+                info!("'{}' already installed and up to date", description);
+                summary.push((format!("{} {}", toolchain, UNCHANGED), "".to_string()));
                 continue;
             };
             (channel.build_download_configs(), hash)
@@ -36,23 +46,42 @@ pub fn update() -> Result<()> {
         for cfg in cfgs {
             let toolchain = Toolchain::from_path(&description.to_string())?;
             match toolchain.add_component(cfg) {
-                Ok(cfg) => writeln!(installed_bins, "- {} {}", cfg.name, cfg.version)?,
-                Err(e) => writeln!(errored_bins, "- {}", e)?,
+                Ok(cfg) => installed_bins.push_str(&format!("  - {} {}\n", cfg.name, cfg.version)),
+                Err(e) => errored_bins.push_str(&format!("  - {}\n", e)),
             };
+        }
+
+        let mut status = String::new();
+        if !installed_bins.is_empty() {
+            status = UPDATED.to_string();
+            installed_bins = format!("  updated components:\n{}", installed_bins);
         }
 
         if errored_bins.is_empty() {
             config.save_hash(&toolchain, &hash)?;
-            info!("\nUpdated:\n{}", installed_bins);
-            info!("\nThe Fuel toolchain is installed and up to date");
-        } else if installed_bins.is_empty() {
-            error!("\nfuelup failed to install:\n{}", errored_bins)
         } else {
-            info!(
-                "\nThe Fuel toolchain is partially installed.\nfuelup failed to install: {}",
-                errored_bins
-            );
+            status = PARTIALLY_UPDATED.to_string();
+            errored_bins = format!("  failed to update:\n{}", errored_bins);
         };
+
+        summary.push((
+            format!("{} {}\n", toolchain, status),
+            format!("{}{}", installed_bins, errored_bins),
+        ));
+    }
+
+    info!("");
+    for (toolchain_info, components_info) in summary {
+        if !toolchain_info
+            .matches(UPDATED)
+            .collect::<String>()
+            .is_empty()
+        {
+            colored_bold(Color::Green, |s| write!(s, "{}", toolchain_info));
+        } else {
+            bold(|s| write!(s, "{}", toolchain_info));
+        }
+        info!("{}", components_info);
     }
 
     Ok(())

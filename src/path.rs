0d5b0433e0a1e5007b3f48a9ea2e02086cae44cd
bingcicle <bingcicle@proton.me>
2022-11-30 11:38:19 +0800

    clearer message

--- src/path.rs
+++ src/path.rs
@@ -84,9 +84,10 @@ pub fn warn_existing_fuel_executables() -> Result<()> {
 
                     if is_executable(&maybe_fuelup_executable) {
                         message.push_str(&format!(
-                            " This takes precedence over '{}' already installed at {}.",
+                            " fuelup found '{}' already installed at {} which will be overshadowed by the copy at {}.",
                             c.name,
-                            &maybe_fuelup_executable.display()
+                            &maybe_fuelup_executable.display(),
+                            path
                         ));
                     } else {
                         message.push_str(&format!(

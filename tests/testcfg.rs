ce6a815cfc57cdf505fb132689a419f877afbe66
bingcicle <bingcicle@proton.me>
2023-01-09 15:03:50 +0800

    newline

--- tests/testcfg.rs
+++ tests/testcfg.rs
@@ -100,7 +100,6 @@ impl TestCfg {
             .env("TERM", "dumb")
             .output()
             .expect("Failed to execute command");
-
         let stdout = String::from_utf8(output.stdout).unwrap();
         let stderr = String::from_utf8(output.stderr).unwrap();
         TestOutput {

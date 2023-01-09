29850509974ea9d7d360150fecee01259cc14a49
bingcicle <bingcicle@proton.me>
2023-01-09 15:43:36 +0800

    prepend github run id and date within build-channel

--- ci/build-channel/src/main.rs
+++ ci/build-channel/src/main.rs
@@ -36,6 +36,10 @@ struct Args {
     pub channel: String,
     /// the TOML file name
     pub out_file: String,
+    /// the GitHub run ID
+    pub github_run_id: String,
+    /// the publish date
+    pub publish_date: String,
     /// Component name [possible values: latest]
     #[clap(value_parser = parse_key_val::<String, Version>)]
     pub packages: Vec<(String, Version)>,
@@ -233,7 +237,14 @@ fn main() -> Result<()> {
 
     println!("writing channel: '{}'", &args.out_file);
 
-    fs::write(&args.out_file, document.to_string())?;
+    let mut channel_str = String::new();
+    channel_str.push_str(&format!(
+        "published_by = \"https://github.com/FuelLabs/fuelup/actions/runs/{}\"\n",
+        args.github_run_id
+    ));
+    channel_str.push_str(&format!("date = \"{}\"\n", args.publish_date));
+    channel_str.push_str(&document.to_string());
+    fs::write(&args.out_file, channel_str.to_string())?;
 
     Ok(())
 }

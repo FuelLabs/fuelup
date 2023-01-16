e6d0900f1d63d621e10f9f65affdae2f3da9e367
bing <bingcicle@proton.me>
2023-01-16 11:26:06 +0800

    fix: default bug (#340)
    * reorder checking for toolchain from settings
    
    * Fix Toolchain::from_settings if settings doesn't exist to begin with
    
    * check for existence of file
    
    * fix: move getting toolchain into match branch
    
    * newline

--- src/ops/fuelup_default.rs
+++ src/ops/fuelup_default.rs
@@ -10,19 +10,26 @@ use crate::{
 };
 
 pub fn default(toolchain: Option<String>) -> Result<()> {
-    let current_toolchain = Toolchain::from_settings()?;
-
     let toolchain = match toolchain {
         Some(toolchain) => toolchain,
         None => {
             let mut result = String::new();
+            let current_toolchain = Toolchain::from_settings()?;
+
             if let Some(to) = ToolchainOverride::from_project_root() {
-                let name = match DistToolchainDescription::from_str(&to.cfg.toolchain.channel) {
-                    Ok(desc) => desc.to_string(),
-                    Err(_) => to.cfg.toolchain.channel,
-                };
-                result.push_str(&format!("{} (override), ", name))
+                let name =
+                    match DistToolchainDescription::from_str(&to.cfg.toolchain.channel.to_string())
+                    {
+                        Ok(desc) => desc.to_string(),
+                        Err(_) => to.cfg.toolchain.channel,
+                    };
+                result.push_str(&format!("{} (override)", name));
+
+                if current_toolchain.exists() {
+                    result.push_str(", ")
+                }
             }
+
             result.push_str(&format!("{} (default)", current_toolchain.name));
 
             info!("{}", result);

67b18690f4948232b7c3582778ca9e2436f09eb3
bing <bingcicle@proton.me>
2023-01-25 14:01:49 +0800

    fix: install overridden components to store instead of to the toolchain dir (#354)
    * install components to store and link to toolchain
    
    * remove println
    
    * fix: do not create 'bin' dir in toolchain
    
    * Move toolchain linking logic into toolchain.rs
    
    * don't use link_to_toolchain
    
    * clippy
    
    * add is_distributed_by_forc check
    
    * handle forc distributed plugins

--- src/store.rs
+++ src/store.rs
@@ -6,8 +6,6 @@ use semver::Version;
 use crate::{
     download::{download_file_and_unpack, unpack_bins, DownloadCfg},
     path::{ensure_dir_exists, store_dir},
-    target_triple::TargetTriple,
-    toolchain_override::ToolchainOverride,
 };
 
 fn component_dirname(component_name: &str, version: &Version) -> String {
@@ -40,28 +38,12 @@ impl Store {
 
     // This function installs a component into a directory within '/.fuelup/store'.
     // The directory is named '<component_name>-<version>', eg. 'fuel-core-0.15.1'.
-    pub(crate) fn install_component(
-        &self,
-        component_name: &str,
-        toolchain_override: &ToolchainOverride,
-    ) -> Result<()> {
-        if let Some(components) = toolchain_override.cfg.components.as_ref() {
-            if let Some(version) = components.get(component_name) {
-                let download_cfg = DownloadCfg::new(
-                    component_name,
-                    TargetTriple::from_component(component_name)?,
-                    Some(version.clone()),
-                )?;
+    pub(crate) fn install_component(&self, cfg: &DownloadCfg) -> Result<Vec<PathBuf>> {
+        let component_dir = self.component_dir_path(&cfg.name, &cfg.version);
 
-                let component_dir = self.component_dir_path(component_name, version);
-
-                ensure_dir_exists(&component_dir)?;
-                download_file_and_unpack(&download_cfg, &component_dir)?;
-                // We ensure that component_dir exists above, so its parent must exist here.
-                unpack_bins(&component_dir, component_dir.parent().unwrap())?;
-            }
-        }
-
-        Ok(())
+        ensure_dir_exists(&component_dir)?;
+        download_file_and_unpack(cfg, &component_dir)?;
+        // We ensure that component_dir exists above, so its parent must exist here.
+        unpack_bins(&component_dir, &component_dir)
     }
 }

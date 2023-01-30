a788cde541d8c0a5ece3778de8c3fdcbecff1c35
bingcicle <bingcicle@proton.me>
2023-01-30 15:23:38 +0800

    refactor std lib caching in toolchain.rs

--- src/toolchain.rs
+++ src/toolchain.rs
@@ -143,6 +143,31 @@ impl FromStr for DistToolchainDescription {
     }
 }
 
+fn cache_sway_std_libs(forc_bin_path: PathBuf) -> Result<()> {
+    let fuelup_tmp_dir = fuelup_tmp_dir();
+    ensure_dir_exists(&fuelup_tmp_dir)?;
+    let temp_project = tempfile::Builder::new().prefix("temp-project").tempdir()?;
+    let temp_project_path = temp_project.path().to_str().unwrap();
+    if Command::new(&forc_bin_path)
+        .args(["init", "--path", temp_project_path])
+        .stdout(std::process::Stdio::null())
+        .status()
+        .is_ok()
+    {
+        info!("Fetching core forc dependencies");
+        if Command::new(forc_bin_path)
+            .args(["check", "--path", temp_project_path])
+            .stdout(std::process::Stdio::null())
+            .status()
+            .is_err()
+        {
+            error!("Failed to fetch core forc dependencies");
+        };
+    };
+
+    Ok(())
+}
+
 #[derive(Debug)]
 pub struct Toolchain {
     pub name: String,
@@ -251,29 +276,7 @@ impl Toolchain {
 
                     // Little hack here to download core and std lib upon installing `forc`
                     if download_cfg.name == component::FORC {
-                        let forc_bin_path = self.bin_path.join(component::FORC);
-
-                        let fuelup_tmp_dir = fuelup_tmp_dir();
-                        ensure_dir_exists(&fuelup_tmp_dir)?;
-                        let temp_project =
-                            tempfile::Builder::new().prefix("temp-project").tempdir()?;
-                        let temp_project_path = temp_project.path().to_str().unwrap();
-                        if Command::new(&forc_bin_path)
-                            .args(["init", "--path", temp_project_path])
-                            .stdout(std::process::Stdio::null())
-                            .status()
-                            .is_ok()
-                        {
-                            info!("Fetching core forc dependencies");
-                            if Command::new(forc_bin_path)
-                                .args(["check", "--path", temp_project_path])
-                                .stdout(std::process::Stdio::null())
-                                .status()
-                                .is_err()
-                            {
-                                error!("Failed to fetch core forc dependencies");
-                            };
-                        };
+                        cache_sway_std_libs(self.bin_path.join(component::FORC))?;
                     };
                 }
                 Err(e) => bail!(

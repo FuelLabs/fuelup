18069cf6a072bfd2df0a1996ff8161804d8b3077
bing <bingcicle@proton.me>
2023-03-24 10:41:10 +0000

    ci: index other components (#414)
    * index latest versions for other components
    
    * final fixes
    
    * fix unrelated clippy
    
    * Handle error from request; document get_latest_version()
    
    Handled error using a match instead.
    
    Also added doc comments on get_latest_version() - it may not be clear
    why we have to rely on the workflow runs API over the releases API.
    
    * more ergonomic way to handle an Ok response

--- tests/expects/mod.rs
+++ tests/expects/mod.rs
@@ -4,7 +4,6 @@ pub fn expect_files_exist(dir: &Path, expected: &[&str]) {
     let mut actual: Vec<String> = dir
         .read_dir()
         .expect("Could not read directory")
-        .into_iter()
         .map(|b| b.unwrap().file_name().to_string_lossy().to_string())
         .collect();
 

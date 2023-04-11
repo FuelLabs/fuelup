75860c53e8b62433f78b7cdf28890be179946d86
bing <bingcicle@proton.me>
2023-04-11 02:18:24 +0000

    chore: deprecate usage of hashes to check download duplication (#431)
    Prior to the 'store' model that we now use, each binary was not cached
    in some local location. We used basic sha256 hashes stored within .fuelup/hashes
    directory as a way to tell fuelup that a toolchain already exists. This is important
    to ensure that users don't waste time re-downloading binaries that they have
    already downloaded once before.
    
    With the store model, we no longer need this since it's much faster now
    to re-install toolchains with duplicate binaries since they are all cached
    in the store. We can now get rid of this hacky way of speeding up the download
    process, since it's built-in to the design.

--- src/config.rs
+++ src/config.rs
@@ -1,55 +1,22 @@
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
-use anyhow::{bail, Result};
-use std::io::{self, ErrorKind};
+use anyhow::Result;
+use std::io;
 
-use crate::file::write_file;
 use crate::fmt::format_toolchain_with_target;
-use crate::path::{ensure_dir_exists, hashes_dir, toolchains_dir};
-use crate::toolchain::{DistToolchainDescription, RESERVED_TOOLCHAIN_NAMES};
+use crate::path::toolchains_dir;
+use crate::toolchain::RESERVED_TOOLCHAIN_NAMES;
 
 pub struct Config {
     toolchains_dir: PathBuf,
-    hashes_dir: PathBuf,
 }
 
 impl Config {
     pub(crate) fn from_env() -> Result<Self> {
         Ok(Self {
             toolchains_dir: toolchains_dir(),
-            hashes_dir: hashes_dir(),
         })
-    }
-
-    pub(crate) fn hashes_dir(&self) -> &Path {
-        self.hashes_dir.as_path()
-    }
-
-    pub(crate) fn hash_matches(
-        &self,
-        description: &DistToolchainDescription,
-        hash: &str,
-    ) -> Result<bool> {
-        let hash_path = self.hashes_dir.join(description.to_string());
-
-        match fs::read_to_string(hash_path) {
-            Ok(h) => Ok(h == hash),
-            Err(e) => match e.kind() {
-                ErrorKind::NotFound => Ok(false),
-                _ => bail!("Failed to read hash from hash file"),
-            },
-        }
-    }
-
-    pub(crate) fn hash_exists(&self, description: &DistToolchainDescription) -> bool {
-        self.hashes_dir.join(description.to_string()).is_file()
-    }
-
-    pub(crate) fn save_hash(&self, toolchain: &str, hash: &str) -> Result<()> {
-        ensure_dir_exists(&self.hashes_dir)?;
-        write_file(&self.hashes_dir.join(toolchain), hash)?;
-        Ok(())
     }
 
     pub(crate) fn list_toolchains(&self) -> Result<Vec<String>> {

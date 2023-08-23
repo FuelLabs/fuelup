abdfa24f49c4457fde5c81c3679f2fc9afc371cb
Chris O'Brien <obrchr96@gmail.com>
2023-08-22 23:09:39 -0500

    list command debug internals are more concise

--- src/commands/nix/link_generator.rs
+++ src/commands/nix/link_generator.rs
@@ -1,13 +1,13 @@
 use crate::commands::fuelup::FuelupCommand;
 
-use super::{install::NixInstallCommand, list::UnlockedAttributePath, FUEL_NIX_LINK};
+use super::{install::NixInstallCommand, list::UnlockedFlakeURL, FUEL_NIX_LINK};
 use anyhow::{bail, Result};
 
 /// Handles getting toolchain or component information to translate
 /// into fuel.nix flake links and info presented to the user.
 pub(crate) trait FlakeLinkInfo {
     /// the name of a toolchain or component
-    fn name(&self) -> &str;
+    fn name(&self) -> String;
     fn get_toolchain(&self) -> Result<FuelToolchain> {
         FuelToolchain::from_str(self.name())
     }
@@ -31,6 +31,7 @@ pub(crate) trait CachixLinkGenerator: FlakeLinkInfo {
             FuelToolchain::Beta2 => "fuel-beta-2",
             FuelToolchain::Beta3 => "fuel-beta-3",
             FuelToolchain::Beta4rc => "fuel-beta-4-rc",
+            FuelToolchain::Unknown => bail!("available distributed toolchains:\n  -latest\n  -nightly\n  -beta-1\n  -beta-2\n  -beta-3\n  -beta-4-rc")
         })
     }
     fn nix_component_suffix(&self) -> Result<(&str, &str)> {
@@ -57,7 +58,7 @@ pub(crate) trait CachixLinkGenerator: FlakeLinkInfo {
             FuelToolchain::Beta2 => "-beta-2",
             FuelToolchain::Beta3 => "-beta-3",
             FuelToolchain::Beta4rc => "-beta-4-rc",
-            FuelToolchain::Unknown => bail!("available distributed toolchains:\n  -latest\n  -nightly\n  -beta-1\n  -beta-2\n  -beta-3\n  -beta-4-rc") 
+            FuelToolchain::Unknown => bail!("available distributed toolchains:\n  -latest\n  -nightly\n  -beta-1\n  -beta-2\n  -beta-3\n  -beta-4-rc")
         };
         Ok((comp, tool))
     }
@@ -71,25 +72,36 @@ pub(crate) trait CachixLinkGenerator: FlakeLinkInfo {
 }
 
 impl FlakeLinkInfo for NixInstallCommand {
-    fn name(&self) -> &str {
-        self.name.as_str()
+    fn name(&self) -> String {
+        self.name.clone()
     }
 }
-impl FlakeLinkInfo for UnlockedAttributePath {
-    fn name(&self) -> &str {
-        let Ok((comp, toolchain)) = split_at_toolchain(self.0.clone()) else {
-
-        };
-        if comp == ".fuel" {
-            toolchain.into()
+impl FlakeLinkInfo for UnlockedFlakeURL {
+    fn name(&self) -> String {
+        let (comp, _) = split_at_toolchain(self.0.clone())
+            .expect("failed to split whitespace of unlocked attribute path");
+        if let Some(index) = comp.find(".fuel") {
+            let (_, comp) = comp.split_at(index);
+            if comp == ".fuel-" {
+                // return the full toolchain name
+                let comp = comp.replace('.', "");
+                let comp = comp.replace('-', "");
+                comp.to_string()
+            } else if comp == ".fuel" {
+                let comp = comp.replace('.', "");
+                comp.to_string()
+            } else {
+                self.0.clone()
+            }
         } else {
-            &comp
+            self.0.clone()
         }
     }
 }
 impl CachixLinkGenerator for NixInstallCommand {}
+impl CachixLinkGenerator for UnlockedFlakeURL {}
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, Hash)]
 pub(crate) enum FuelToolchain {
     Latest,
     Nightly,
@@ -101,14 +113,14 @@ pub(crate) enum FuelToolchain {
 }
 
 impl FuelToolchain {
-    fn from_str(s: &str) -> Result<Self> {
+    fn from_str(s: String) -> Result<Self> {
         Ok(match s.to_lowercase().as_str() {
-            "latest" => Self::Latest,
-            "nightly" => Self::Nightly,
-            "beta-1" | "beta1" => Self::Beta1,
-            "beta-2" | "beta2" => Self::Beta2,
-            "beta-3" | "beta3" => Self::Beta3,
-            "beta-4-rc" | "beta-4rc" | "beta4rc" => Self::Beta4rc,
+            "latest" | "fuel" => Self::Latest,
+            "nightly" | "fuel-nightly" => Self::Nightly,
+            "beta-1" | "beta1" | "fuel-beta-1" => Self::Beta1,
+            "beta-2" | "beta2" | "fuel-beta-2" => Self::Beta2,
+            "beta-3" | "beta3" | "fuel-beta-3" => Self::Beta3,
+            "beta-4-rc" | "beta-4rc" | "beta4rc" | "fuel-beta-4-rc" => Self::Beta4rc,
             _ => bail!("available distributed toolchains:\n  -latest\n  -nightly\n  -beta-1\n  -beta-2\n  -beta-3\n  -beta-4-rc") 
         })
     }
@@ -116,8 +128,8 @@ impl FuelToolchain {
         *self == FuelToolchain::Latest
     }
 }
-impl<'a> From<&'a str> for FuelToolchain {
-    fn from(s: &'a str) -> Self {
+impl From<String> for FuelToolchain {
+    fn from(s: String) -> Self {
         match s.to_lowercase().as_str() {
             "latest" => Self::Latest,
             "nightly" => Self::Nightly,
@@ -160,7 +172,7 @@ pub(crate) enum FuelComponent {
     SwayVim,
 }
 impl FuelComponent {
-    fn from_str(s: &str) -> Result<(Self, FuelToolchain)> {
+    fn from_str(s: String) -> Result<(Self, FuelToolchain)> {
         let (comp, tool) = split_at_toolchain(s.to_lowercase())?;
         // remove the excess '-' between the comp and toolchain vers
         let comp = if !tool.is_latest() {
@@ -196,10 +208,10 @@ please form a valid component, like so: fuel-core-beta-3"
 pub(crate) fn split_at_toolchain(s: String) -> Result<(String, FuelToolchain)> {
     let (comp, tool) = if let Some(index) = s.find("beta") {
         let (comp, tool) = s.split_at(index);
-        (comp.into(), FuelToolchain::from_str(tool)?)
+        (comp.into(), FuelToolchain::from_str(tool.to_string())?)
     } else if let Some(index) = s.find("nightly") {
         let (comp, tool) = s.split_at(index);
-        (comp.into(), FuelToolchain::from_str(tool)?)
+        (comp.into(), FuelToolchain::from_str(tool.to_string())?)
     } else {
         (s, FuelToolchain::Latest)
     };
--- src/commands/nix/list.rs
+++ src/commands/nix/list.rs
@@ -1,4 +1,7 @@
-use super::{link_generator::FlakeLinkInfo, NIX_CMD, PROFILE_LIST_ARGS};
+use super::{
+    link_generator::{split_at_toolchain, FlakeLinkInfo, FuelToolchain},
+    NIX_CMD, PROFILE_LIST_ARGS,
+};
 use crate::commands::nix::nix_info;
 use anyhow::{bail, Result};
 use clap::Parser;
@@ -8,40 +11,82 @@ use tracing::info;
 #[derive(Debug, Parser)]
 pub struct NixListCommand;
 
+/// A binary package installed by the fuel.nix flake.
 #[derive(Debug)]
 pub(crate) struct NixBinaryInfo {
     pub(crate) name: String,
     pub(crate) index: u32,
-    pub(crate) unlocked_attr_path: String,
-    pub(crate) locked_attr_path: String,
+    pub(crate) flake_attribute: Option<String>,
+    pub(crate) unlocked_flake_url: String,
+    pub(crate) locked_flake_url: String,
     pub(crate) nix_store_path: String,
 }
 impl NixBinaryInfo {
     fn new(
-        name: &str,
+        name: String,
         index: &str,
-        unlocked_attr_path: &str,
-        locked_attr_path: &str,
+        flake_attribute: Option<String>,
+        unlocked_flake_url: &str,
+        locked_flake_url: &str,
         nix_store_path: &str,
     ) -> Self {
         Self {
-            name: name.into(),
+            name,
             index: index.parse::<u32>().unwrap(),
-            unlocked_attr_path: unlocked_attr_path.into(),
-            locked_attr_path: unlocked_attr_path.into(),
+            flake_attribute,
+            unlocked_flake_url: unlocked_flake_url.into(),
+            locked_flake_url: unlocked_flake_url.into(),
             nix_store_path: nix_store_path.into(),
         }
     }
 }
 
-pub(crate) struct UnlockedAttributePath(pub(crate) String);
+/// Used to get the information about a package since it has a
+/// more reliable structure than either the flake attribute, which
+/// isn't present on my machine (M1 macbook pro, nix (Nix) 2.15.0),
+/// or the nix store path which contains a randomly produced hash.
+pub(crate) struct UnlockedFlakeURL(pub(crate) String);
+impl<'a> From<&'a str> for UnlockedFlakeURL {
+    fn from(s: &'a str) -> Self {
+        Self(s.to_string())
+    }
+}
+impl UnlockedFlakeURL {
+    fn split_at_toolchain(&self) -> Result<(String, FuelToolchain)> {
+        if let Some(index) = self.0.find(".fuel") {
+            let (_, tool) = split_at_toolchain(self.0.split_at(index).1.split_at(1).1.to_string())
+                .expect("failed to get toolchain from unlocked attribute path");
+            Ok((self.name(), tool))
+        } else {
+            bail!("could not get toolchain info from attribute path")
+        }
+    }
+    fn split_at_component(&self) -> Result<(String, FuelToolchain)> {
+        if let Some(index) = self.0.find(".forc") {
+            split_at_toolchain(self.0.split_at(index).1.split_at(1).1.to_string())
+        } else {
+            bail!("could not get toolchain info from attribute path")
+        }
+    }
+}
 
 #[derive(Debug)]
-pub(crate) struct NixBinaryList(pub(crate) HashMap<String, Vec<NixBinaryInfo>>);
+pub(crate) struct NixBinaryList(pub(crate) HashMap<FuelToolchain, Vec<NixBinaryInfo>>);
 
+/// Currently this collects a static 4 values from the stdout string
+/// produced by `nix profile list`, however, in some cases we may actually
+/// get 5. The four present are the index, unlocked flake link, locked flake link
+/// and nix store path. The fifth would come after the index and is the flake
+/// attribute which for some reason doesn't show up presently but _could_ in the
+/// future.
+///
+/// To avoid breakage we could look to collect at every index, then to be sure
+/// of each value we can perform checks to see what that data holds. eg, an index
+/// can be parsed as an integer, the flake links will start with "github:fuellabs" and
+/// the nix store path will start with "nix/store/".
 impl From<Vec<u8>> for NixBinaryList {
     fn from(v: Vec<u8>) -> Self {
-        let mut map: HashMap<String, Vec<NixBinaryInfo>> = HashMap::new();
+        let mut map: HashMap<FuelToolchain, Vec<NixBinaryInfo>> = HashMap::new();
         let stdout = String::from_utf8_lossy(&v);
         let mut stdout_iter = stdout.split_whitespace();
         let mut count = stdout_iter.clone().count();
@@ -57,23 +102,43 @@ impl From<Vec<u8>> for NixBinaryList {
             count -= 4;
         }
         for inner_vec in outer_vec.iter() {
-            let unlocked_attr_path = inner_vec[1];
-            if let Some(index) = unlocked_attr_path.find(".fuel") {
-                let (_, name) = unlocked_attr_path.split_at(index);
+            let unlocked_attr_path = UnlockedFlakeURL::from(inner_vec[1]);
+            if unlocked_attr_path.is_toolchain() {
+                let (name, toolchain) = unlocked_attr_path
+                    .split_at_toolchain()
+                    .expect("failed to get pkg info from unlocked attribute path");
                 let nix_bin = NixBinaryInfo::new(
                     name,
                     inner_vec[0],
-                    unlocked_attr_path,
+                    None, /* see comment on this impl */
+                    inner_vec[1],
                     inner_vec[2],
                     inner_vec[3],
                 );
-                if let Some(index) = name.find('-') {
-                    let (_, toolchain) = name.clone().split_at(index);
-                    match map.get_mut(toolchain) {
-                        Some(nix_bins) => nix_bins.push(nix_bin),
-                        None => {
-                            map.insert(toolchain.into(), vec![nix_bin]);
-                        }
+
+                match map.get_mut(&toolchain) {
+                    Some(nix_bins) => nix_bins.push(nix_bin),
+                    None => {
+                        map.insert(toolchain, vec![nix_bin]);
+                    }
+                }
+            } else if unlocked_attr_path.is_component() {
+                let (name, toolchain) = unlocked_attr_path
+                    .split_at_component()
+                    .expect("failed to get pkg info from unlocked attribute path");
+                let nix_bin = NixBinaryInfo::new(
+                    name,
+                    inner_vec[0],
+                    None, /* see comment on this impl */
+                    inner_vec[1],
+                    inner_vec[2],
+                    inner_vec[3],
+                );
+
+                match map.get_mut(&toolchain) {
+                    Some(nix_bins) => nix_bins.push(nix_bin),
+                    None => {
+                        map.insert(toolchain, vec![nix_bin]);
                     }
                 }
             }
@@ -86,7 +151,7 @@ pub fn nix_list(_command: NixListCommand) -> Result<()> {
     match Command::new(NIX_CMD).args(PROFILE_LIST_ARGS).output() {
         Ok(output) => {
             let nix_bin_list = NixBinaryList::from(output.stdout);
-            println!("{nix_bin_list:?}");
+            dbg!(nix_bin_list);
             // nix_info!(output);
             Ok(())
         }

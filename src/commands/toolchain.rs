364e8eaaabc320b7fdfa8b14e276aa340a61b861
Sophie Dankel <47993817+sdankel@users.noreply.github.com>
2023-09-18 22:45:23 -0700

    Update toolchain help message (#486)
    * Update toolchain.rs
    
    * update override string

--- src/commands/toolchain.rs
+++ src/commands/toolchain.rs
@@ -22,7 +22,7 @@ pub enum ToolchainCommand {
 
 #[derive(Debug, Parser)]
 pub struct InstallCommand {
-    /// Toolchain name [possible values: latest, beta-1, beta-2, beta-3, nightly]
+    /// Toolchain name [possible values: latest, beta-1, beta-2, beta-3, beta-4, nightly]
     pub name: String,
 }
 

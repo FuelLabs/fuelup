9e5fa2c11c71bc469b02855e385520324be048b3
Sophie Dankel <47993817+sdankel@users.noreply.github.com>
2023-11-28 12:55:59 -0800

    Write logs to filesystem (#522)
    * Write logs to filesystem
    
    * ansiterm
    
    * clean up
    
    * update tests
    
    * update more tests

--- tests/check.rs
+++ tests/check.rs
@@ -7,35 +7,41 @@ use testcfg::FuelupState;
 
 #[test]
 fn fuelup_check() -> Result<()> {
-    let latest = format!("latest-{}\n", TargetTriple::from_host().unwrap());
-    let beta_1 = format!("beta-1-{}\n", TargetTriple::from_host().unwrap());
+    let latest = format!("latest-{}", TargetTriple::from_host().unwrap());
+    let beta_1 = format!("beta-1-{}", TargetTriple::from_host().unwrap());
     let forc = "forc -";
     let fuel_core = "fuel-core -";
     let fuel_indexer = "fuel-indexer -";
     testcfg::setup(FuelupState::Empty, &|cfg| {
         let output = cfg.fuelup(&["check"]);
-        assert!(!output.stdout.contains(&latest));
-        assert!(!output.stdout.contains(forc));
-        assert!(!output.stdout.contains(fuel_core));
-        assert!(!output.stdout.contains(fuel_indexer));
+        let stripped = strip_ansi_escapes::strip(output.stdout);
+        let stdout = String::from_utf8_lossy(&stripped);
+        assert!(!stdout.contains(&latest));
+        assert!(!stdout.contains(forc));
+        assert!(!stdout.contains(fuel_core));
+        assert!(!stdout.contains(fuel_indexer));
     })?;
 
     // Test that only the 'latest' toolchain shows.
     testcfg::setup(FuelupState::LatestAndCustomInstalled, &|cfg| {
         let output = cfg.fuelup(&["check"]);
-        assert!(output.stdout.contains(&latest));
-        assert!(output.stdout.contains(forc));
-        assert!(output.stdout.contains(fuel_core));
-        assert!(output.stdout.contains(fuel_indexer));
+        let stripped = strip_ansi_escapes::strip(output.stdout);
+        let stdout = String::from_utf8_lossy(&stripped);
+        assert!(stdout.contains(&latest));
+        assert!(stdout.contains(forc));
+        assert!(stdout.contains(fuel_core));
+        assert!(stdout.contains(fuel_indexer));
     })?;
 
     // Test that toolchain names with '-' inside are parsed correctly.
     testcfg::setup(FuelupState::Beta1Installed, &|cfg| {
         let output = cfg.fuelup(&["check"]);
-        assert!(output.stdout.contains(&beta_1));
-        assert!(output.stdout.contains(forc));
-        assert!(output.stdout.contains(fuel_core));
-        assert!(!output.stdout.contains(fuel_indexer));
+        let stripped = strip_ansi_escapes::strip(output.stdout);
+        let stdout = String::from_utf8_lossy(&stripped);
+        assert!(stdout.contains(&beta_1));
+        assert!(stdout.contains(forc));
+        assert!(stdout.contains(fuel_core));
+        assert!(!stdout.contains(fuel_indexer));
     })?;
 
     Ok(())

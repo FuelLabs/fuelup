c48760ef7e190f92ac98e631a944a7988ca33e1f
Kaya Gökalp <kaya.gokalp@fuel.sh>
2024-01-23 08:13:07 +0300

    feat: add beta-5 support (#541)
    * feat: add beta-5 support
    
    * chore: fmt
    
    * point to beta-5 as latest

--- src/constants.rs
+++ src/constants.rs
@@ -6,12 +6,13 @@ pub const FUELS_VERSION_FILE: &str = "fuels_version";
 
 pub const CHANNEL_LATEST_URL: &str =
     "https://raw.githubusercontent.com/FuelLabs/fuelup/gh-pages/channel-fuel-beta-4.toml";
-pub const CHANNEL_LATEST_FILE_NAME: &str = "channel-fuel-beta-4.toml";
+pub const CHANNEL_LATEST_FILE_NAME: &str = "channel-fuel-beta-5.toml";
 pub const CHANNEL_NIGHTLY_FILE_NAME: &str = "channel-fuel-nightly.toml";
 pub const CHANNEL_BETA_1_FILE_NAME: &str = "channel-fuel-beta-1.toml";
 pub const CHANNEL_BETA_2_FILE_NAME: &str = "channel-fuel-beta-2.toml";
 pub const CHANNEL_BETA_3_FILE_NAME: &str = "channel-fuel-beta-3.toml";
 pub const CHANNEL_BETA_4_FILE_NAME: &str = "channel-fuel-beta-4.toml";
+pub const CHANNEL_BETA_5_FILE_NAME: &str = "channel-fuel-beta-5.toml";
 
 pub const DATE_FORMAT: &[FormatItem] = format_description!("[year]-[month]-[day]");
 pub const DATE_FORMAT_URL_FRIENDLY: &[FormatItem] = format_description!("[year]/[month]/[day]");

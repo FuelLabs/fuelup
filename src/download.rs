fe8144bcaf66d37909c75c1a81f3ffd8cefed3e8
YusongWang <admin@wangyusong.com>
2024-03-05 20:46:28 +0800

    Add uppercase http_proxy environment

--- src/download.rs
+++ src/download.rs
@@ -102,6 +102,16 @@ pub fn build_agent() -> Result<ureq::Agent> {
         };
     }
 
+    if let Ok(proxy) = env::var("HTTP_PROXY") {
+        return match ureq::Proxy::new(&proxy) {
+            Ok(proxy) => Ok(agent_builder.proxy(proxy).build()),
+            Err(err) => {
+                error!("Failed to build proxy with http_proxy={}, {}", proxy, err);
+                Err(err.into())
+            }
+        };
+    }
+
     Ok(agent_builder.build())
 }
 

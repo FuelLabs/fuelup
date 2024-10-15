34f9bfdecd6a44d550cddf5188e135e4745bb6ec
Sophie <47993817+sdankel@users.noreply.github.com>
2024-10-15 12:56:25 -0700

    point latest to mainnet

--- src/constants.rs
+++ src/constants.rs
@@ -6,10 +6,10 @@ pub const FUELS_VERSION_FILE: &str = "fuels_version";
 
 pub const CHANNEL_LATEST_URL: &str =
     "https://raw.githubusercontent.com/FuelLabs/fuelup/gh-pages/channel-fuel-testnet.toml";
-pub const CHANNEL_LATEST_FILE_NAME: &str = "channel-fuel-testnet.toml";
 pub const CHANNEL_NIGHTLY_FILE_NAME: &str = "channel-fuel-nightly.toml";
 pub const CHANNEL_TESTNET_FILE_NAME: &str = "channel-fuel-testnet.toml";
 pub const CHANNEL_MAINNET_FILE_NAME: &str = "channel-fuel-mainnet.toml";
+pub const CHANNEL_LATEST_FILE_NAME: &str = CHANNEL_MAINNET_FILE_NAME;
 
 pub const DATE_FORMAT: &[FormatItem] = format_description!("[year]-[month]-[day]");
 pub const DATE_FORMAT_URL_FRIENDLY: &[FormatItem] = format_description!("[year]/[month]/[day]");

3bedbae6252180592fddc667d8fa7f6813cee59b
JoshuaBatty <joshpbatty@gmail.com>
2025-03-03 13:48:46 +1100

    use anyhow

--- ci/update-compiler-explorer/src/main.rs
+++ ci/update-compiler-explorer/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use serde_yaml::{self, Value};
 use std::{env, fs, path::Path, process::Command};
 use tempfile::tempdir;
@@ -9,28 +10,28 @@ const LIBRARIES_YAML_PATH: &str = "bin/yaml/libraries.yaml";
 const AMAZON_PROPERTIES_PATH: &str = "etc/config/sway.amazon.properties";
 const STD_LIB_PATH: &str = "/opt/compiler-explorer/libs/sway/std";
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let new_version = env::args().nth(1).ok_or("Missing version argument")?;
+fn main() -> Result<()> {
+    let new_version = env::args().nth(1).context("Missing version argument")?;
 
-    let temp_dir = tempdir()?;
-    let infra_repo_path = temp_dir.path().join("infra");
-    Command::new("git")
-        .args([
-            "clone",
-            &format!("https://github.com/{}.git", INFRA_REPO),
-            infra_repo_path.to_str().unwrap(),
-        ])
-        .status()?;
+    let clone_repo = |repo: &str, dir_name: &str| -> Result<_> {
+        let temp_dir = tempdir()?;
+        let repo_path = temp_dir.path().join(dir_name);
+        Command::new("git")
+            .args([
+                "clone",
+                &format!("https://github.com/{}.git", repo),
+                repo_path.to_str().unwrap(),
+            ])
+            .status()
+            .with_context(|| format!("Failed to clone {} repo", dir_name))?;
 
-    let temp_dir = tempdir()?;
-    let compiler_explorer_path = temp_dir.path().join("compiler-explorer");
-    Command::new("git")
-        .args([
-            "clone",
-            &format!("https://github.com/{}.git", COMPILER_EXPLORER_REPO),
-            compiler_explorer_path.to_str().unwrap(),
-        ])
-        .status()?;
+        // Return both temp_dir and repo_path to keep temp_dir alive
+        Ok((temp_dir, repo_path))
+    };
+
+    // Clone the repositories
+    let (_, infra_repo_path) = clone_repo(INFRA_REPO, "infra")?;
+    let (_, compiler_explorer_path) = clone_repo(COMPILER_EXPLORER_REPO, "compiler-explorer")?;
 
     update_amazon_properties(&compiler_explorer_path, &new_version)?;
     update_sway_yaml(&infra_repo_path, &new_version)?;
@@ -54,14 +55,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 // Updates the targets section of the sway.yaml file with the given version
-fn update_sway_yaml(repo_path: &Path, version: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn update_sway_yaml(repo_path: &Path, version: &str) -> Result<()> {
     let path = repo_path.join(SWAY_YAML_PATH);
     let content = fs::read_to_string(&path)?;
     let mut yaml = serde_yaml::from_str::<Value>(&content)?;
 
     let targets = yaml["compilers"]["sway"]["targets"]
         .as_sequence_mut()
-        .ok_or("Invalid YAML structure")?;
+        .context("Invalid YAML structure")?;
     if !targets.iter().any(|v| v.as_str() == Some(version)) {
         targets.push(Value::String(version.to_string()));
         fs::write(&path, serde_yaml::to_string(&yaml)?)?;
@@ -71,17 +72,14 @@ fn update_sway_yaml(repo_path: &Path, version: &str) -> Result<(), Box<dyn std::
 }
 
 // Updates the targets section of sway in the libraries.yaml file with the given version
-fn update_libraries_yaml(
-    repo_path: &Path,
-    version: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
+fn update_libraries_yaml(repo_path: &Path, version: &str) -> Result<()> {
     let path = repo_path.join(LIBRARIES_YAML_PATH);
     let content = fs::read_to_string(&path)?;
     let mut yaml = serde_yaml::from_str::<Value>(&content)?;
 
     let targets = yaml["libraries"]["sway"]["std"]["targets"]
         .as_sequence_mut()
-        .ok_or("Invalid YAML structure")?;
+        .context("Invalid YAML structure")?;
     if !targets.iter().any(|v| v.as_str() == Some(version)) {
         targets.push(Value::String(version.to_string()));
         fs::write(&path, serde_yaml::to_string(&yaml)?)?;
@@ -90,10 +88,8 @@ fn update_libraries_yaml(
     Ok(())
 }
 
-fn update_amazon_properties(
-    repo_path: &Path,
-    version: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
+// Adds a new compiler version to the amazon.properties file
+fn update_amazon_properties(repo_path: &Path, version: &str) -> Result<()> {
     let path = repo_path.join(AMAZON_PROPERTIES_PATH);
     let mut lines = fs::read_to_string(&path)?
         .lines()
